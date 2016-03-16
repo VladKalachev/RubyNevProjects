e9d4c42641641feb92cde181bb60b33c7984cf71
Vlad Panda <vlvete1990@yandex.ru>
2016-03-16 16:38:20 +0300

    first commit1

---
+++ calc.rb
@@ -0,0 +1,40 @@
+#puts 10 + 10
+# простой комментарий
+
+=begin
+многострочный комментарий	
+
+
+
+puts "Hello World!"
+
+puts 10 + 10
+
+def method_h
+	puts 365*24
+end
+
+method_h
+
+puts "Hello" + ' ' + 'Pither'
+
+puts "Salli "*10
+
+puts 'I\'ll'
+puts 'text\\text'
+
+
+
+a = 1
+name = " Vlad"
+b = "Hello"
+puts b + name
+
+
+a = 5
+b = "4"
+puts a. + b.to_f # преобразуем sting to integer
+=end
+
+puts 10
+puts "10 Piter to".to_i
